aa6fb6fc96a52bab394f3fa22a6ae7aed8aaec5c
KimGenius <geniusk1047@naver.com>
2021-10-27 10:44:38 +0900

    board 추가 & 가져오기 API 작업

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/about-test')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const Board = mongoose.model('Board', {
+  name: String,
+  content: String,
+  createdAt: {type: Date, default: Date.now},
+})
+
+function generateRandString() {
+  return Math.random().toString(36).substr(2, 20)
+}
+
+app.post('/boards', async (req, res) => {
+  const name = generateRandString()
+  const board = new Board({name , content: generateRandString() })
+  await board.save()
+  res.send(`${name} 추가 완료`)
+})
+
+app.get('/boards', async(req, res) => {
+  res.json(await Board.find())
 })
 
 app.listen(port, () => {
