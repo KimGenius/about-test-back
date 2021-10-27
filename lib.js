e4813f6d9763dfeec7846e1b8dcb5bfa9c292017
KimGenius <geniusk1047@naver.com>
2021-10-27 18:32:56 +0900

    max < min

--- lib.js
+++ lib.js
@@ -12,6 +12,9 @@ function getRandomTargetMember(targetMember = []) {
 }
 
 function getRandomInt(min, max) {
+  if (max < min) {
+    return "min 값은 max 보다 클 수 없습니다."
+  }
   min = Math.ceil(min)
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min)) + min
